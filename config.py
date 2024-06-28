6b72af777278b19405c4c82044b8fe59facfc945
روجر <104784913+rogerpq@users.noreply.github.com>
2024-06-28 17:03:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = getenv("API_HASH") or None
 BOT_TOKEN = getenv("BOT_TOKEN")
 OWNER_ID = int(getenv("OWNER_ID"))
 
-MONGO_DB_URI = os.environ.get("SCALINGO_MONGODB_URL", None)
+MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
 MUST_JOIN = getenv("MUST_JOIN", "Repthon")
 
 PORT = os.environ.get("PORT", None)

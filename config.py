ea8165471bdf5b94fc1d4c4c75708831d67b7813
روجر <104784913+rogerpq@users.noreply.github.com>
2024-06-30 13:56:06 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,13 @@
-import os
 from os import getenv
 from dotenv import load_dotenv
 
 load_dotenv()
 
 API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH") or None
+API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
 OWNER_ID = int(getenv("OWNER_ID"))
 
-MONGO_DB_URI = os.environ.get("SCALINGO_MONGO_URL", None)
+MONGO_DB_URI = getenv("MONGO_DB_URI")
 MUST_JOIN = getenv("MUST_JOIN", "Repthon")
-
-PORT = os.environ.get("PORT", None)

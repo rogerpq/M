b8897d3cb63811c040875d992b11f0ea49f34bd9
روجر <104784913+rogerpq@users.noreply.github.com>
2024-07-02 20:20:12 +0300

    Update bot_users.py

--- StringGenBot/bot_users.py
+++ StringGenBot/bot_users.py
@@ -1,16 +1,22 @@
-from pyrogram.types import Message
+from StringGenBot.db.users_sql import Users, num_users
+from StringGenBot.db import SESSION
 from pyrogram import Client, filters
-
+from pyrogram.types import Message
 from config import OWNER_ID
-from StringGenBot.db.users import add_served_user, get_served_users
 
 
-@Client.on_message(filters.private & ~filters.service, group=1)
+@Client.on_message(~filters.edited & ~filters.service, group=1)
 async def users_sql(_, msg: Message):
-    await add_served_user(msg.from_user.id)
+    if msg.from_user:
+        q = SESSION.query(Users).get(int(msg.from_user.id))
+        if not q:
+            SESSION.add(Users(msg.from_user.id))
+            SESSION.commit()
+        else:
+            SESSION.close()
 
 
-@Client.on_message(filters.user(OWNER_ID) & filters.command("stats"))
+@Client.on_message(filters.user(OWNER_ID) & ~filters.edited & filters.command("stats"))
 async def _stats(_, msg: Message):
-    users = len(await get_served_users())
-    await msg.reply_text(f"» ᴄᴜʀʀᴇɴᴛ sᴛᴀᴛs ᴏғ sᴛʀɪɴɢ ɢᴇɴ ʙᴏᴛ :\n\n {users} ᴜsᴇʀs", quote=True)
+    users = await num_users()
+    await msg.reply(f"Total Users : {users}", quote=True)

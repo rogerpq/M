7e2ebe609519aa62476a113bf3c2a1376c5b4807
روجر <104784913+rogerpq@users.noreply.github.com>
2024-07-02 20:24:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,23 +4,25 @@ ENVIRONMENT = os.environ.get("ENVIRONMENT", "ANYTHING")
 
 if ENVIRONMENT:
     try:
-        API_ID = int(os.environ.get("API_ID", 0))
+        API_ID = int(os.environ.get("API_ID", 6))
     except ValueError:
         raise Exception("Your API_ID is not a valid integer.")
     API_HASH = os.environ.get("API_HASH", None)
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
     DATABASE_URL = os.environ.get("DATABASE_URL", None)
-    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql") 
+    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
+    OWNER_ID = os.environ.get("OWNER_ID", "5502537272")
     MUST_JOIN = os.environ.get("MUST_JOIN", "Repthon")
     if MUST_JOIN.startswith("@"):
         MUST_JOIN = MUST_JOIN.replace("@", "Repthon")
 else:
     # Fill the Values
-    API_ID = 0
+    API_ID = 6
     API_HASH = ""
     BOT_TOKEN = ""
     DATABASE_URL = ""
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
-    MUST_JOIN = ""
+    OWNER_ID = "5502537272"
+    MUST_JOIN = "Repthon"
     if MUST_JOIN.startswith("@"):
         MUST_JOIN = MUST_JOIN[1:]

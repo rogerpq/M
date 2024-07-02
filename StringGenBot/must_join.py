89544182a4e672cf7fd842a2a150b80d633ab0e9
روجر <104784913+rogerpq@users.noreply.github.com>
2024-07-02 22:52:54 +0300

    Update must_join.py

--- StringGenBot/must_join.py
+++ StringGenBot/must_join.py
@@ -1,25 +1,24 @@
-from config import MUST_JOIN
-
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+from config import MUST_JOIN
 
 
 @Client.on_message(filters.incoming & filters.private, group=-1)
-async def must_join_channel(Client: Client, msg: Message):
+async def must_join_channel(bot: Client, msg: Message):
     if not MUST_JOIN:
         return
     try:
         try:
-            await Client.get_chat_member(MUST_JOIN, Client.from_user.id)
+            await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
         except UserNotParticipant:
             if MUST_JOIN.isalpha():
                 link = "https://t.me/" + MUST_JOIN
             else:
-                chat_info = await Client.get_chat(MUST_JOIN)
+                chat_info = await bot.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
             try:
-                await Client.reply_photo(
+                await bot.reply_photo(
                     photo="https://telegra.ph/file/5eb95caa40488cea43cd3.jpg", caption=f"» عـذرًا عـزيـزي عليك الاشتـراك في قنـاة البـوت !",
                     reply_markup=InlineKeyboardMarkup(
                         [
@@ -29,7 +28,7 @@ async def must_join_channel(Client: Client, msg: Message):
                         ]
                     )
                 )
-                await Client.stop_propagation()
+                await bot.stop_propagation()
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:

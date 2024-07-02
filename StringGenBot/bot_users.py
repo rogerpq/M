fc1f6f9ae6094732d349e7bafc1abc810a787842
روجر <104784913+rogerpq@users.noreply.github.com>
2024-07-02 22:27:21 +0300

    Update bot_users.py

--- StringGenBot/bot_users.py
+++ StringGenBot/bot_users.py
@@ -5,7 +5,7 @@ from pyrogram.types import Message
 from config import OWNER_ID
 
 
-@Client.on_message(~filters.edited & ~filters.service, group=1)
+@Client.on_message(~filters.private & ~filters.service, group=1)
 async def users_sql(_, msg: Message):
     if msg.from_user:
         q = SESSION.query(Users).get(int(msg.from_user.id))
@@ -16,7 +16,7 @@ async def users_sql(_, msg: Message):
             SESSION.close()
 
 
-@Client.on_message(filters.user(OWNER_ID) & ~filters.edited & filters.command("stats"))
+@Client.on_message(filters.user(OWNER_ID) & filters.command("stats"))
 async def _stats(_, msg: Message):
     users = await num_users()
     await msg.reply(f"Total Users : {users}", quote=True)

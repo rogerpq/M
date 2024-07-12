6b344eda290139e352fd2208236f60d97d14b5aa
روجر <104784913+rogerpq@users.noreply.github.com>
2024-07-13 00:47:49 +0300

    Update start.py

--- StringGenBot/start.py
+++ StringGenBot/start.py
@@ -8,7 +8,7 @@ from config import OWNER_ID
 @Client.on_message(filters.private & filters.incoming & filters.command("start"))
 async def start(bot: Client, msg: Message):
 	user = await bot.get_me()
-	mention = user["mention"]
+	mention = user ["mention"]
 	await bot.send_message(
 		msg.chat.id,
 		Data.START.format(msg.from_user.mention, mention),
